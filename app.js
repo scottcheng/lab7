4ecf8575116329afccc11ee926e673d43210218c
Scott Cheng <me@scottcheng.com>
2014-02-19 14:06:10 -0800

    Add MONGOHQ_URI in app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var project = require('./routes/project');
 // MAKE SURE TO CHANGE THE NAME FROM 'lab7' TO ... IN OTHER PROJECTS
 var local_database_name = 'lab7';
 var local_database_uri  = 'mongodb://localhost/' + local_database_name
-var database_uri = process.env.MONGOLAB_URI || local_database_uri
+var database_uri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || local_database_uri
 mongoose.connect(database_uri);
 
 var app = express();
